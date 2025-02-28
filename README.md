ef425efb10ed08d19b2bf75f8322ac32a1d72437
westzerosilver <oooiooop9988@gmail.com>
2025-03-01 01:36:01 +0900

    chore: remove all cached files and apply .gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# i-gacha-backend
--- src/main/java/team/backend/apiPayload/code/BaseErrorCode.java
+++
@@ -1,8 +0,0 @@
-package team.backend.apiPayload.code;
-
-public interface BaseErrorCode {
-
-    public ErrorReasonDTO getReason();
-
-    public ErrorReasonDTO getReasonHttpStatus();
-}
