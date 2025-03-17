4be728c2610383ab36fd5d7c54cfadcf1fc5b30a
westzerosilver <oooiooop9988@gmail.com>
2025-03-17 20:27:23 +0900

    Chore: JWT token을 cookie에만 저장

--- src/main/java/team/backend/jwt/JWTFilter.java
+++ src/main/java/team/backend/jwt/JWTFilter.java
@@ -51,12 +51,34 @@ System.out.println("requestUri: " + requestUri);
             return;
         }
 
+        String authorization = null;
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+
+            System.out.println(cookie.getName());
+            if (cookie.getName().equals("Authorization")) {
+
+                authorization = cookie.getValue();
+            }
+        }
+
+        //Authorization 헤더 검증
+        if (authorization == null) {
+
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
         // 헤더에서 access키에 담긴 토큰을 꺼냄
-        String accessToken = request.getHeader("access");
+//        String accessToken = request.getHeader("access");
 
         // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
         try {
-            jwtUtil.isExpired(accessToken);
+            jwtUtil.isExpired(authorization);
         } catch (ExpiredJwtException e) {
             //response body
             PrintWriter writer = response.getWriter();
@@ -73,7 +95,7 @@ System.out.println("requestUri: " + requestUri);
         }
 
         //토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(accessToken)) {
+        if (jwtUtil.isExpired(authorization)) {
 
             filterChain.doFilter(request, response);
 
@@ -82,8 +104,8 @@ System.out.println("requestUri: " + requestUri);
         }
 
         //토큰에서 username과 role 획득
-        String username = jwtUtil.getUsername(accessToken);
-        String role = jwtUtil.getRole(accessToken);
+        String username = jwtUtil.getUsername(authorization);
+        String role = jwtUtil.getRole(authorization);
 
         //userDTO를 생성하여 값 set
         UserDTO userDTO = new UserDTO();
