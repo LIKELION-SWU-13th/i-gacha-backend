52d76f5394f4a30d7310969e034ce4756ffa15d7
yujining3827 <j_yujin_swu22@naver.com>
2025-03-21 18:49:45 +0900

    event_id로 응답

--- src/main/java/team/backend/dto/EventDto/EventCreateDto.java
+++ src/main/java/team/backend/dto/EventDto/EventCreateDto.java
@@ -1,11 +1,13 @@
 package team.backend.dto.EventDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EventCreateDto {
+    @JsonProperty("event_id")
     private Long eventId;
 
     public EventCreateDto(Long eventId) {
