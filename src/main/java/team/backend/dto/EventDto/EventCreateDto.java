64ef93e093907ad297ca0a1d735f42203997e9de
yujining3827 <j_yujin_swu22@naver.com>
2025-04-05 06:36:11 +0900

    fix: 이벤트 생성 dto 수정

--- src/main/java/team/backend/dto/EventDto/EventCreateDto.java
+++ src/main/java/team/backend/dto/EventDto/EventCreateDto.java
@@ -2,13 +2,12 @@ package team.backend.dto.EventDto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class EventCreateDto {
+
     @JsonProperty("event_id")
-    private Long eventId;
+    private final Long eventId;
 
     public EventCreateDto(Long eventId) {
         this.eventId = eventId;
