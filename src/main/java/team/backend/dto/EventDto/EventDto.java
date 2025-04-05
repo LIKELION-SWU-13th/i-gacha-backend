360a3d13a7388918bf97f2d5549ed0b4e2fc2159
yujining3827 <j_yujin_swu22@naver.com>
2025-04-05 18:44:14 +0900

    fix : EventDto.java

--- src/main/java/team/backend/dto/EventDto/EventDto.java
+++ src/main/java/team/backend/dto/EventDto/EventDto.java
@@ -2,8 +2,11 @@ package team.backend.dto.EventDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
